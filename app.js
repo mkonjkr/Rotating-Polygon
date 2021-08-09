c33805327f1e25649dc5e3601d2a302419e0c820
LukeJeong <mkonjkr@gmail.com>
2021-08-09 15:33:12 -0700

    #Rotating Polygon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { Polygon } from "./polygon.js";
 
 class App {
     constructor() {
+        //set canvas
         this.canvas = document.createElement('canvas');
         document.body.appendChild(this.canvas);
         this.ctx = this.canvas.getContext('2d');
@@ -11,6 +12,14 @@ class App {
         
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
+        
+        this.isDown = false;
+        this.moveX = 0;
+        this.offsetX = 0;
+
+        document.addEventListener('pointerdown', this.onDown.bind(this), false);
+        document.addEventListener('pointermove', this.onMove.bind(this), false);
+        document.addEventListener('pointerup', this.onUp.bind(this), false);
 
         window.requestAnimationFrame(this.animate.bind(this));
     }
@@ -21,13 +30,18 @@ class App {
         this.canvas.width = this.stageWidth * this.pixelRatio;
         this.canvas.height = this.stageHeight * this.pixelRatio;
         this.ctx.scale(this.pixelRatio, this.pixelRatio);
-
+        
         this.polygon = new Polygon(
+            // middle of width
             this.stageWidth / 2,
-            this.stageHeight / 2,
-            this.stageHeight / 3,
-            3
+            // middle of Height
+            this.stageHeight + (this.stageHeight / 4),
+            // size of Polygon
+            this.stageHeight / 1.5,
+            // number of sides
+            15
         );
+
     }
 
     animate() {
@@ -35,7 +49,25 @@ class App {
 
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
 
-        this.polygon.animate(this.ctx);
+        this.moveX *= 0.92;
+        this.polygon.animate(this.ctx, this.moveX);
+    }
+
+    onDown(e) {
+        this.isDown = true;
+        this.moveX = 0;
+        this.offsetX = e.clientX;
+    }
+
+    onMove(e) {
+        if (this.isDown) {
+            this.moveX = e.clientX - this.offsetX;
+            this.offsetX = e.clientX;
+        }
+    }
+
+    onUp(e) {
+        this.isDown = false;
     }
 }
 
